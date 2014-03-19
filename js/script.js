6ee97bd16cf8760d2797a8cb54b995bfa75ba418
gkthegr8 <gkthegr8@gmail.com>
2014-03-19 17:26:41 +0530

    Lamp finished

---
+++ js/script.js
@@ -0,0 +1,6 @@
+$('.lamp-switch').click(function(){
+	$('.lamp-switch-hanging').toggleClass('moveDown');
+	$('.lamp-switch').toggleClass('moveDown');
+	$('.lamp-bulb').toggleClass('lightUp');
+	$('.bulbBottom').toggleClass('lightUp');
+})
